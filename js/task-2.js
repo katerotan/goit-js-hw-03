7dcdebce6bc45111f82d3685c13660dba45aed5b
katerotan <katerinarotan@gmail.com>
2023-11-01 16:15:03 +0200

    Task 3 Done

--- js/task-2.js
+++ js/task-2.js
@@ -32,7 +32,6 @@ function makeArray(firstArray, secondArray, maxLength) {
 }
 
 
-
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
